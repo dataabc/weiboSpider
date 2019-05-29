72ba69e4621b0c3e9a067a855e9ad30c4ce9d2c3
chenlei <chillychen1991@gmail.com>
2019-05-29 21:03:31 +0800

    feat: 添加对cookie是否正确的判断

--- weiboSpider.py
+++ weiboSpider.py
@@ -64,6 +64,8 @@ class Weibo:
             selector = self.deal_html(url)
             nickname = selector.xpath("//title/text()")[0]
             self.nickname = nickname[:-3]
+            if self.nickname == u"登录 - 新":
+                sys.exit(u"cookie错误或已过期,请按照README中方法重新获取")
             print(u"用户昵称: " + self.nickname)
         except Exception as e:
             print("Error: ", e)
