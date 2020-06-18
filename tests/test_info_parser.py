747e0481f64bb7fc299d4d7e88dbf4e62ede2484
Zhengyang Song <songzy_thu@163.com>
2020-06-19 01:08:19 +0800

    tests[#168]: add test for info_parser

---
+++ tests/test_info_parser.py
@@ -0,0 +1,12 @@
+from unittest.mock import patch
+
+from .util import mock_request_get_content
+from weibo_spider.parser.info_parser import InfoParser
+
+
+@patch('requests.get', mock_request_get_content)
+def test_page_parser():
+    info_parser = InfoParser(cookie="", user_id="1669879400")
+    user = info_parser.extract_user_info()
+    # With info_parser, we can only get the nickname.
+    assert (user.nickname == "Dear-迪丽热巴")
