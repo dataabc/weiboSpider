17d8b6309de13ab36c4ed9a099826d222c371175
Zhengyang Song <songzy_thu@163.com>
2020-06-18 23:42:06 +0800

    feat: add a mechanism to generate mock test data for url requests.

--- weibo_spider/parser/util.py
+++ weibo_spider/parser/util.py
@@ -1,15 +1,42 @@
 import sys
 import traceback
+import hashlib
 
 import requests
 from lxml import etree
+
+# Set GENERATE_TEST_DATA to True when generating test data.
+GENERATE_TEST_DATA = False
+TEST_DATA_DIR = 'tests/testdata'
+URL_MAP_FILE = "url_map.json"
+
+
+def hash_url(url):
+    return hashlib.sha224(url.encode('utf8')).hexdigest()
 
 
 def handle_html(cookie, url):
     """处理html"""
     try:
-        html = requests.get(url, cookies=cookie).content
-        selector = etree.HTML(html)
+        resp = requests.get(url, cookies=cookie)
+
+        if GENERATE_TEST_DATA:
+            import io
+            import json
+            import os
+
+            resp_file = os.path.join(TEST_DATA_DIR, "%s.html" % hash_url(url))
+            with io.open(resp_file, "w") as f:
+                f.write(resp.text)
+
+            with io.open(os.path.join(TEST_DATA_DIR, URL_MAP_FILE), 'r+') as f:
+                url_map = json.loads(f.read())
+                url_map[url] = resp_file
+                f.seek(0)
+                f.write(json.dumps(url_map, indent=4, ensure_ascii=False))
+                f.truncate()
+
+        selector = etree.HTML(resp.content)
         return selector
     except Exception as e:
         print("Error: ", e)
