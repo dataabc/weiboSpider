27a5f045558cb42e7081f431b7d31aa6e5f3c69a
dataabc <chillychen1991@gmail.com>
2020-12-05 21:00:03 +0800

    fix: 修复没有安装kafka而出错的bug
    Issue #266

--- weibo_spider/writer/kafka_writer.py
+++ weibo_spider/writer/kafka_writer.py
@@ -1,18 +1,26 @@
 import json
 import logging
-
-from kafka import KafkaProducer
+import sys
 
 from .writer import Writer
+
 logger = logging.getLogger('spider.kafka_writer')
 
 
 class KafkaWriter(Writer):
-
     def __init__(self, kafka_config):
+        try:
+            from kafka import KafkaProducer
+        except ImportError:
+            logger.warning(
+                u'系统中可能没有安装kafka库，请先运行 pip install kafka-python ，再运行程序')
+            sys.exit()
+
         self.kafka_config = kafka_config
-        self.producer = KafkaProducer(bootstrap_servers=str(kafka_config['bootstrap-server']).split(','),
-                                 value_serializer=lambda m: json.dumps(m,ensure_ascii=False).encode('UTF-8'))
+        self.producer = KafkaProducer(
+            bootstrap_servers=str(kafka_config['bootstrap-server']).split(','),
+            value_serializer=lambda m: json.dumps(m, ensure_ascii=False
+                                                  ).encode('UTF-8'))
         self.weibo_topics = list(kafka_config['weibo_topics'])
         self.user_topics = list(kafka_config['user_topics'])
         logger.info('{}', kafka_config)
